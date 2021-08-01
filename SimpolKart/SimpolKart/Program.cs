b75941b534876ec7ef52703137baece7169f900c
7Weniamin7 <88266791+7Weniamin7@users.noreply.github.com>
2021-08-01 12:56:09 +0500

    SimbolKart
    Simbol kart

---
+++ SimpolKart/SimpolKart/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SimpolKart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string k1, k2, k3;
+            Console.WriteLine("Доступные картинки нарисованиые символами:");
+            Console.WriteLine("SMAL");
+            Console.WriteLine("Small");
+            Console.WriteLine("Kroll");
+            Console.WriteLine("Mahh");
+            k1 = Convert.ToString(Console.ReadLine());
+            k2 = Convert.ToString(Console.ReadLine());
+            if (k1 == "Small")
+            {
+                Console.WriteLine("(>'•')");
+            }
+            if (k1 == "SMAL")
+            {
+                Console.WriteLine("(・_＋)");
+            }
+            if (k1 == "Kroll")
+            {
+                Console.WriteLine ("o,,,o   /),/)");
+                Console.WriteLine("( ' : ' )( ' ; ' )");
+                Console.WriteLine("(,,)-(,,)(,,)-(,,)");
+               
+            }
+            if (k1 == "Mahh")
+            {
+                Console.WriteLine("     ____()()");
+                Console.WriteLine("   /           @@");
+                Console.WriteLine("~~~~~m__m._>o");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
